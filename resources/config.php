185e5e048759c82dc5b1b33ed02d7dacede666d9
jrondorf <github@jrondorf.de>
2025-04-14 22:00:58 +0200

    update

--- resources/config.php
+++ resources/config.php
@@ -7,10 +7,10 @@
  */
 
 /* MySQL configuration */ 
-define("SCIMIFY_DB_USERNAME", "root");
-define("SCIMIFY_DB_PASSWORD", "");
+define("SCIMIFY_DB_USERNAME", "scimify");
+define("SCIMIFY_DB_PASSWORD", "scimify");
 define("SCIMIFY_DB_SERVER", "localhost");
 define("SCIMIFY_DB_NAME","scimify");
 
 /* Other configuration */
-error_reporting(0);
+//error_reporting(0);
